4ff5e9d98b34856fbb5e9485ca59ec4e83464c98
Feng Li <m@feng.li>
2023-07-08 20:50:07 +0800

    Update Spark setup

--- setup-spark-env.sh
+++ setup-spark-env.sh
@@ -1,12 +1,19 @@
 export SPARK_HOME=$HOME/.local/mapreduce/spark
-export SPARK_CONF_DIR=$HOME/.local/mapreduce/spark-on-slurm/spark_conf
 
-# Find Java installation home based on the system java executable
-export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
 
-py4j=$(find "$SPARK_HOME/python/lib/" -maxdepth 1 -type f -name "*src.zip")
-export PYTHONPATH=$SPARK_HOME/python:$py4j:$PYTHONPATH
+## Spark setup only when Spark binaries exist
+if [ -d $SPARK_HOME ]; then
 
-export PATH=$SPARK_HOME/bin:$PATH
+    export SPARK_CONF_DIR=$HOME/.local/mapreduce/spark-on-slurm/spark_conf
 
-export PYARROW_IGNORE_TIMEZONE=1
+    # Find Java installation home based on the system java executable
+    export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
+
+    py4j=$(find "$SPARK_HOME/python/lib/" -maxdepth 1 -type f -name "*src.zip")
+    export PYTHONPATH=$SPARK_HOME/python:$py4j:$PYTHONPATH
+
+    export PATH=$SPARK_HOME/bin:$PATH
+
+    export PYARROW_IGNORE_TIMEZONE=1
+
+fi
